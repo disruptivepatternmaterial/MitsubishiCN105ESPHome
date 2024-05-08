71bef6d49cc0961ae29ceb9f085fd5955c41493d
ntableman <nathan@tableman.com>
2024-05-08 07:45:44 -0700

    s

--- components/cn105/climateControls.cpp
+++ components/cn105/climateControls.cpp
@@ -264,13 +264,6 @@ void CN105Climate::updateAction() {
     case climate::CLIMATE_MODE_FAN_ONLY:
         this->action = climate::CLIMATE_ACTION_FAN;
         break;
-    case climate::CLIMATE_MODE_HEAT_COOL:
-        //need to change this to the real status by reading the status!
-        this->setActionIfOperatingTo(
-            (this->current_temperature > this->target_temperature ?
-                climate::CLIMATE_ACTION_COOLING :
-                climate::CLIMATE_ACTION_HEATING));
-        break;
     default:
         this->action = climate::CLIMATE_ACTION_OFF;
     }
